36bbfac4ff095cb2c640bc42a7d2df594acf0632
Xela10001 <alexandre.gazur@ua.pt>
2023-05-14 19:52:15 +0100

    Created base springboot application with mysql-compatible classes.

---
+++ src/main/java/pt/ua/tqsproject/Status.java
@@ -0,0 +1,5 @@
+package pt.ua.tqsproject;
+
+public enum Status {
+	NOT_ACCEPTED, DELIVERING, WAITING_FOR_PICKUP, COMPLETED, RETURNED, MISSING
+}
---
+++ src/main/java/pt/ua/tqsproject/TqsProjectApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.tqsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqsProjectApplication.class, args);
+	}
+
+}

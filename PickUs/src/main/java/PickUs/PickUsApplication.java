c53c00e3ec22569550d1ff4b607dede208336153
zzzzz <rjsp15@ua.pt>
2023-05-17 01:25:52 +0100

    Added base Spring Boot project, SonarCloud CI (workflow) and updated QA manual to include SonarCloud CI (#4)

---
+++ PickUs/src/main/java/PickUs/PickUsApplication.java
@@ -0,0 +1,13 @@
+package PickUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickUsApplication.class, args);
+	}
+
+}
---
+++ PickUs/src/test/java/PickUs/PickUsApplicationTests.java
@@ -0,0 +1,13 @@
+package PickUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

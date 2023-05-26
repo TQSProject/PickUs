1f538c1cd2b29409fc2a186739bc82c5c7c3ff81
zzzzz <ricardopinto15@hotmail.com>
2023-05-26 23:53:02 +0100

    refactored acps

--- PickUs/src/main/java/tqs/PickUs/entities/ACP.java
+++ PickUs/src/main/java/tqs/PickUs/entities/ACP.java
@@ -4,22 +4,38 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "ACPs")
 public class ACP {
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 	private String name;
 	private String city;
 	
 	public ACP() {
 	
 	}
-	
+
 	public ACP(String name, String city) {
 		this.name = name;
 		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "ACP [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 	public String getName() {
@@ -37,4 +53,5 @@ public class ACP {
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 }
--- PickUs/src/main/java/tqs/PickUs/repositories/ACPsRepository.java
+++ PickUs/src/main/java/tqs/PickUs/repositories/ACPsRepository.java
@@ -5,5 +5,12 @@ import org.springframework.stereotype.Repository;
 import tqs.PickUs.entities.ACP;
 
 @Repository
-public interface ACPRepository extends JpaRepository<ACP, Long> {
+public interface ACPsRepository extends JpaRepository<ACP, Integer> {
+    public ACP findById(int id);
+
+    public ACP findByName(String name);
+
+    public ACP findByCity(String city);
+
+    public ACP findByNameAndCity(String name, String city);
 }

d16c6439725cf4ddddf4ecc3848cee8f8408d16b
Xela10001 <alexandre.gazur@ua.pt>
2023-05-30 17:52:29 +0100

    Created OrderService tests

--- PickUs/src/test/java/tqs/PickUs/Orders/OrdersServiceTests.java
+++ PickUs/src/test/java/tqs/PickUs/Orders/OrdersServiceTests.java
@@ -1,23 +1,23 @@
 package tqs.PickUs.Orders;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-import tqs.PickUs.entities.Order;
-import tqs.PickUs.repositories.ACPsRepository;
-import tqs.PickUs.repositories.OrdersRepository;
-import tqs.PickUs.services.ACPsService;
-import tqs.PickUs.entities.ACP;
-import tqs.PickUs.entities.ACPStatus;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tqs.PickUs.entities.ACP;
+import tqs.PickUs.entities.Order;
+import tqs.PickUs.entities.OrderStatus;
+import tqs.PickUs.repositories.OrdersRepository;
+import tqs.PickUs.services.OrdersService;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Test scenario: verify the logic of the Service, mocking the response of the
@@ -26,11 +26,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
  */
 @ExtendWith(MockitoExtension.class)
 public class OrdersServiceTests {
-	@Mock(lenient = true)
+	@Mock(strictness = Mock.Strictness.LENIENT)
 	private OrdersRepository ordersRepository;
 	
 	@InjectMocks
-	private ACPsService acpsService;
+	private OrdersService ordersService;
 	
 	private Order leite, amendoas;
 	private List<Order> orders;
@@ -54,7 +54,9 @@ public class OrdersServiceTests {
 	@BeforeEach
 	public void setup() {
 		leite = createOrder1();
+		leite.setId(1);
 		amendoas = createOrder2();
+		leite.setId(2);
 		orders = List.of(leite, amendoas);
 		
 		updateMockedRepo();
@@ -64,78 +66,59 @@ public class OrdersServiceTests {
 		Mockito.when(ordersRepository.findAll()).thenReturn(orders);
 		
 		Mockito.when(ordersRepository.findById(leite.getId())).thenReturn(leite);
-		
 		Mockito.when(ordersRepository.findById(amendoas.getId())).thenReturn(amendoas);
 	}
 	
 	@Test
-	public void testGetACPsWithFilters() {
-		// params: name, city, status
-		HashMap<String, String> params = new HashMap<String, String>();
-		params.put("city", "Aveiro");
-		List<ACP> acpsAveiro = acpsService.getACPs(params);
-		Assertions.assertEquals(acpsAveiro.size(), 2);
-		Assertions.assertTrue(acpsAveiro.containsAll(Arrays.asList(continenteGlicinias, fnacAveiro)));
+	public void testGetOrdersWithFilters() {
+		HashMap<String, String> params = new HashMap<>();
+		params.put("store", "FNAC");
+		List<Order> ordersFNAC = ordersService.getOrders(params);
+		Assertions.assertEquals(ordersFNAC.size(), 2);
+		Assertions.assertTrue(ordersFNAC.containsAll(Arrays.asList(leite, amendoas)));
 		
-		params = new HashMap<String, String>();
-		params.put("name", "Seaside");
-		List<ACP> acpsSeaside = acpsService.getACPs(params);
-		Assertions.assertEquals(acpsSeaside.size(), 2);
-		Assertions.assertTrue(acpsSeaside.containsAll(Arrays.asList(seasideSetubal, seasidePorto)));
+		params = new HashMap<>();
+		params.put("buyer", "Afonso");
+		List<Order> ordersAfonso = ordersService.getOrders(params);
+		Assertions.assertEquals(ordersAfonso.size(), 2);
+		Assertions.assertTrue(ordersAfonso.containsAll(Arrays.asList(leite, amendoas)));
 		
-		fnacAveiro.setStatus(ACPStatus.APPROVED);
-		seasideSetubal.setStatus(ACPStatus.APPROVED);
-		updateMockedRepo();
+		params = new HashMap<>();
+		params.put("product", "Amendoas");
+		List<Order> ordersAmendoas = ordersService.getOrders(params);
+		Assertions.assertEquals(ordersAmendoas.size(), 1);
+		Assertions.assertTrue(ordersAmendoas.contains(amendoas));
 		
-		params = new HashMap<String, String>();
-		params.put("status", "APPROVED");
-		List<ACP> acpsApproved = acpsService.getACPs(params);
-		Assertions.assertEquals(acpsApproved.size(), 2);
-		Assertions.assertTrue(acpsApproved.containsAll(Arrays.asList(fnacAveiro, seasideSetubal)));
-		
-		params = new HashMap<String, String>();
-		params.put("name", "Seaside");
-		params.put("city", "Porto");
-		List<ACP> acpsSeasidePorto = acpsService.getACPs(params);
-		Assertions.assertEquals(acpsSeasidePorto.size(), 1);
-		Assertions.assertTrue(acpsSeasidePorto.contains(seasidePorto));
-		
-		params = new HashMap<String, String>();
-		params.put("name", "Seaside");
-		params.put("city", "Porto");
-		params.put("status", "APPROVED");
-		List<ACP> acpsSeasidePortoApproved = acpsService.getACPs(params);
-		Assertions.assertEquals(acpsSeasidePortoApproved.size(), 0);
 		
 	}
 	
 	@Test
-	public void testUpdateAcpStatus() {
+	public void testUpdateOrderStatus() {
 		// Return the argument
 		Mockito.when(ordersRepository.save(Mockito.any())).thenAnswer(i -> i.getArguments()[0]);
 		
-		for (ACP acp : acps)
-			Assertions.assertEquals(acp.getStatus(), ACPStatus.WAITING_ADMIN_APPROVAL);
+		for (Order order : orders)
+			Assertions.assertEquals(order.getStatus(), OrderStatus.WAITING_ADMIN_APPROVAL);
 		
-		ACP savedFnacAveiro = acpsService.updateACP(fnacAveiro.getId(), ACPStatus.APPROVED);
-		ACP savedSeasideSetubal = acpsService.updateACP(seasideSetubal.getId(), ACPStatus.REFUSED);
+		Order savedLeite = ordersService.updateOrder(leite.getId(), OrderStatus.DELIVERING);
+		Order savedAmendoas = ordersService.updateOrder(amendoas.getId(), OrderStatus.DELIVERING);
 		
-		fnacAveiro.setStatus(ACPStatus.APPROVED);
-		seasideSetubal.setStatus(ACPStatus.REFUSED);
+		leite.setStatus(OrderStatus.DELIVERING);
+		amendoas.setStatus(OrderStatus.DELIVERING);
 		updateMockedRepo();
 		
-		Assertions.assertEquals(acpsService.getACPById(continenteGlicinias.getId()).getStatus(),
-				ACPStatus.WAITING_ADMIN_APPROVAL);
-		Assertions.assertNotNull(savedSeasideSetubal);
-		Assertions.assertNotNull(fnacAveiro);
-		Assertions.assertEquals(savedFnacAveiro, fnacAveiro);
-		Assertions.assertEquals(savedFnacAveiro.getStatus(), fnacAveiro.getStatus());
-		Assertions.assertEquals(savedSeasideSetubal, seasideSetubal);
-		Assertions.assertEquals(savedSeasideSetubal.getStatus(), seasideSetubal.getStatus());
+		Assertions.assertEquals(ordersService.getOrderById(leite.getId()).getStatus(),
+				OrderStatus.DELIVERING);
+		Assertions.assertNotNull(savedAmendoas);
+		Assertions.assertNotNull(savedLeite);
+		Assertions.assertEquals(savedLeite, leite);
+		Assertions.assertEquals(savedLeite.getStatus(), leite.getStatus());
+		Assertions.assertEquals(savedAmendoas, amendoas);
+		Assertions.assertEquals(savedAmendoas.getStatus(), amendoas.getStatus());
 		
 		Mockito.verify(ordersRepository, VerificationModeFactory.times(2)).save(Mockito.any());
-		Mockito.verify(ordersRepository, VerificationModeFactory.times(1)).save(fnacAveiro);
-		Mockito.verify(ordersRepository, VerificationModeFactory.times(1)).save(seasideSetubal);
+		Mockito.verify(ordersRepository, VerificationModeFactory.times(1)).save(leite);
+		Mockito.verify(ordersRepository, VerificationModeFactory.times(1)).save(amendoas);
 	}
 	
 }
